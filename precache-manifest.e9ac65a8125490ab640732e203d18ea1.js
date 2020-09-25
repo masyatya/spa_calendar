6a232edc0bab413b0b52ff18ef22ad9134ba2db1
Maxym Chernyavskiy <masyatya@gmail.com>
2020-09-25 10:57:05 +0300

    Updates

---
+++ precache-manifest.e9ac65a8125490ab640732e203d18ea1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd11973d1b15258834c4e4b7003d4ff",
+    "url": "/spa_calendar/index.html"
+  },
+  {
+    "revision": "cd61f7c8364617fb3505",
+    "url": "/spa_calendar/static/css/main.28678a83.chunk.css"
+  },
+  {
+    "revision": "9c5bf27ba9b38f84b2d2",
+    "url": "/spa_calendar/static/js/2.4031dd3d.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/spa_calendar/static/js/2.4031dd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd61f7c8364617fb3505",
+    "url": "/spa_calendar/static/js/main.60d2583c.chunk.js"
+  },
+  {
+    "revision": "902ff4d8bbda5d196cbd",
+    "url": "/spa_calendar/static/js/runtime-main.e4caea75.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/spa_calendar/static/media/home-bg.37a75b54.jpg"
+  },
+  {
+    "revision": "89fca7ad6c611609d651ced2ab1ab6f6",
+    "url": "/spa_calendar/static/media/logo.89fca7ad.png"
+  }
+]);
